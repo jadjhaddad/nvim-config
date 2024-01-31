cfa018a6c1a3fafe9adf03362cfbd082257c6acf
Jad Haddad <jadjihadhaddad@gmail.com>
2024-01-31 17:09:24 +0200

    fixed oil shortcuts and added autopairs

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
-  'stevearc/oil.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+    'stevearc/oil.nvim',
+    name = "oil",
+    opts = {
+        use_default_keymaps = true,
+        view_options = {
+            show_hidden = true,
+        }
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
 }
--- lua/remap.lua
+++ lua/remap.lua
@@ -1,4 +1,4 @@
 -- vim remaps
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>ee", "<CMD>Oil<CR>")
-vim.keymap.set("n", "-", "<CMD>Oil<CR>")
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", {})
+vim.keymap.set("n", "<leader>ee", "<CMD>Oil<CR>", {})
